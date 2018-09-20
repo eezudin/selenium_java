6dd83a85b386b465ca738634598cbf231609d17f
eezudin <eezudin87@gmail.com>
2018-09-21 02:59:35 +0400

    LoginTest

--- selenium_java/src/test/java/ru/sw_testing/selenium_training/LoginTest.java
+++ selenium_java/src/test/java/ru/sw_testing/selenium_training/LoginTest.java
@@ -4,14 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.textMatches;
-import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class LoginTest {
 
