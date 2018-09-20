ca0ae8ddc77d243852204a53f593dec6eb1124c6
eezudin <eezudin87@gmail.com>
2018-09-21 00:34:43 +0400

    initial commit

---
+++ selenium_java/src/test/java/ru/sw_testing/selenium_training/MyFirstTest.java
@@ -0,0 +1,41 @@
+package ru.sw_testing.selenium_training;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 20);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://www.google.com/ncr");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        //not stable
+        //driver.findElement(By.name("btnK")).click();
+        driver.findElement(By.name("btnK")).sendKeys(Keys.ENTER);
+        wait.until(titleIs("webdriver - Google Search"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
