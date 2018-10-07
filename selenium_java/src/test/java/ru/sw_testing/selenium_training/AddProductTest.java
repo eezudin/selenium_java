13d0224795c679220403f19d30853bca3918f43c
eezudin <eezudin87@gmail.com>
2018-10-08 01:02:49 +0400

    Add product test

--- selenium_java/src/test/java/ru/sw_testing/selenium_training/AddProductTest.java
+++ selenium_java/src/test/java/ru/sw_testing/selenium_training/AddProductTest.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.io.File;
 
-import java.io.IOException;
 import java.util.List;
 
 public class AddProductTest {
