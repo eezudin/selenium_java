4fa4066f542790496baf3fc8d63e4fb782024397
eezudin <eezudin87@gmail.com>
2018-10-20 17:31:25 +0400

    New window test

---
+++ selenium_java/src/test/java/ru/sw_testing/selenium_training/NewWindowTest.java
@@ -0,0 +1,73 @@
+package ru.sw_testing.selenium_training;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Set;
+
+public class NewWindowTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public ExpectedCondition<String> anyWindowOtherThan(Set<String> oldWindows) {
+        return new ExpectedCondition<String>() {
+            public String apply(WebDriver driver) {
+                Set<String> handles = driver.getWindowHandles();
+                handles.removeAll(oldWindows);
+                return handles.size() > 0 ? handles.iterator().next() : null;
+            }
+        };
+    }
+
+    public boolean checkNewWindows() {
+        List<WebElement> elements = driver.findElements(By.xpath("//i[@class='fa fa-external-link']"));
+        if (elements.size() != 0) {
+            for (int i = 0; i < elements.size(); i++) {
+                String originalWindow = driver.getWindowHandle();
+                Set<String> existingWindows = driver.getWindowHandles();
+                driver.findElement(By.xpath("//i[@class='fa fa-external-link']")).click();
+                String newWindow = wait.until(anyWindowOtherThan(existingWindows));
+                driver.switchTo().window(newWindow);
+                driver.close();
+                driver.switchTo().window(originalWindow);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 20);
+    }
+
+    @Test
+    public void newWindowTest() {
+        driver.get("http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+        driver.findElement(By.xpath("//a[contains(@href,'countries')]")).click();
+        driver.findElement(By.xpath("//a[@title='Edit']")).click();
+        Assert.assertTrue(checkNewWindows());
+
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
